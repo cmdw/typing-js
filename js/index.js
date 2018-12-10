ab00f72ddf6f82ab41bfe92a251aa5d0c832eda7
Dreamagination <s.k.candra@gmail.com>
2018-12-10 21:59:49 +0700

    first

---
+++ js/index.js
@@ -0,0 +1,9 @@
+$(function() {
+  new Typed('#typed', {
+		strings: ["HARDWARE","SOFTWARE","DATABASE","TECHNOLOGY!"],
+		typeSpeed: 100,
+    backDelay: 1500,
+    loop: true,
+    callback: function(){}
+	});
+});
